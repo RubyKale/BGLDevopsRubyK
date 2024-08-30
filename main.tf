a900ab64b70069f490aa1f5167b68d457293de3b
RubyKale <164979387+RubyKale@users.noreply.github.com>
2024-08-30 17:33:58 +1000

    Update main.tf
    IC

--- main.tf
+++ main.tf
@@ -1,39 +1,45 @@
-#!/bin/bash
-#define a variable with cutoff value
-
-cutoff_timestamp="2022-04-12 13:00:00"
-
-#store the output in the file 
-
-output_file="instance_ids.txt"
-
-
-# Define the URL of the CSV file
-CSV_URL="https://raw.githubusercontent.com/BGLCorp/devops-interview-tests/main/ec2-instances.csv"
-
-# Fetch and process the CSV file
-#awk used to read the columns with comman separataion -F,  and a variable is passed using -v
-curl -s $CSV_URL | awk -F, -v cutoff="$cutoff_timestamp" '
-{
-    # Remove quotes from the timestamp field
-    gsub(/"/, "", $2)
-
-    # Combine time and date properly from the CSV fields
-    # The timestamp field should be in the format HH:MM:SS YYYY-MM-DD
-    time_date = $2
-    
-    # Rearrange time_date to YYYY-MM-DD HH:MM:SS for comparison
-    split(time_date, arr, " ")
-    formatted_timestamp = arr[2] " " arr[1]
-
-    # Compare the formatted timestamp with the cutoff timestamp
-    if (formatted_timestamp < cutoff) {
-        gsub(/"/, "", $1)  # Removes quotes from the first column
-        print $1           # Print the InstanceId (first field)
-        #print formatted_timestamp  # Print the timestamp (second field)
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.65.0"
     }
-}'> "$output_file"
+  }
+}
 
-cat $output_file
+provider "aws" {
+  region = "ap-southeast-2"  # Specify your desired AWS region
+}
 
+locals {
+  users = {
+    "user1" = {
+      username = "jerome"
+      groups   = ["group1", "group2"]
+    },
+    "user2" = {
+      username = "marc"
+      groups   = ["group2", "group3"]
+    }
+  }
+  groups_lists = [for user in local.users : user.groups]
+  flat_groups  = flatten(local.groups_lists) # flatten the list
+  groups = toset(local.flat_groups) # convert list to set
+}
 
+resource "aws_iam_group" "groups" {
+  for_each = local.groups
+  name = each.value
+}
+
+resource "aws_iam_user" "users" {
+  for_each = local.users
+  name = each.value.username
+}
+
+resource "aws_iam_user_group_membership" "memberships" {
+  for_each = local.users
+
+  user = aws_iam_user.users[each.key].name
+  groups = [for g in each.value.groups : aws_iam_group.groups[g].name] 
+}
